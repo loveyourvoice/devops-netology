a2991da488be4395039dcd04e5a3b79caed0752e
Vladislav Plotnikov <plotnikov@technolight.ru>
2023-07-27 07:43:33 +0300

    merge git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # display command line options
-
 count=1
 for param in "$@"; do
-    echo "\$@ Parameter #$count = $param"
+    echo "Next parameter: $param"
     count=$(( $count + 1 ))
 done
-
-echo "====="
